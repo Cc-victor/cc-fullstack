d4988ec5137db3308dfa4599c8b4514db21ccd18
cc <2292483588@qq.com>
2021-03-04 21:32:30 +0800

    2021/3/4

--- react2/antd/antd-todolist/src/App.js
+++ react2/antd/antd-todolist/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 // import { Button } from 'antd';
-import TodoList from './TodoList'
+import Notes from './components/Notes';
+// import TodoList from './TodoList'
 
 function App() {
   return (
     <div className="App">
-      <TodoList />
+      <Notes />
       {/* <Button type="primary">点击</Button> */}
     </div>
   );
