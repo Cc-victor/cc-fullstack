a6e4c33ddd6f8a01919def3f0f09566f36e1d75a
game-members <2292483588@qq.com>
2021-04-10 16:37:42 +0800

    vue

---
+++ VUE3.0/shop/src/store/index.js
@@ -0,0 +1,14 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+  },
+  mutations: {//放更改状态的函数
+    
+  },
+  actions: {
+    //提交mutations里面的方法 提交到VUEX里面去
+  },
+  modules: {
+  }
+})
